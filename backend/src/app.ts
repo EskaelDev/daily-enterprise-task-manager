531aceeb2916c3894fabf4cc44b785d1af831117
tomek <eskaeldev@gmail.com>
2020-12-08 14:06:39 +0100

    cors as typescript

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,15 +2,14 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 import * as bodyParser from 'body-parser';
 import "reflect-metadata";
 import { useExpressServer } from "routing-controllers";
+import cors from 'cors';
 
 export default class App {
     public app: express.Application;
     public port: number;
-    public cors;
 
     constructor(port: number) {
-        const express = require('express');
-        this.cors = require('cors');
+
         this.app = express();
         this.port = port;
 
@@ -19,7 +18,7 @@ export default class App {
     }
 
     private initializeMiddlewares() {
-        this.app.use(bodyParser.json(), this.cors());
+        this.app.use(bodyParser.json(), cors());
     }
 
     private initializeControllers() {
