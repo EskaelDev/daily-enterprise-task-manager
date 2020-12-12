b4f34150187383f0c573c6bf9476bbd482a0f35d
tomek <eskaeldev@gmail.com>
2020-12-12 22:32:06 +0100

    sign in, jwt auth, authorized create user, update README.md
    close #27
    close #30
    close #31
    close #37

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,14 @@
+import 'reflect-metadata';
 import App from './app';
 import UsersController from './api/users/users.controller'
 import dotenv from 'dotenv'
+import Container from 'typedi';
+import { useContainer } from 'routing-controllers';
 
 dotenv.config();
 const port = process.env.PORT || 5000;
+
+useContainer(Container);
 
 const app = new App(+port);
 
--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,4 +1,32 @@
-export default class AuthorizationService{
+import { Service } from "typedi";
+import crypto from 'crypto'
+import User from '../api/users/user.service'
+import UserService from "../api/users/user.service";
+import jwt from 'jsonwebtoken'
+@Service()
+export default class AuthorizationService {
+
+    /**
+     *
+     */
+    constructor(private userService: UserService) {
+
+    }
+    public GenerateToken() {
+        crypto.randomBytes(64).toString('hex')
+    }
 
 
+    public async Login(login: string, password: string) {
+
+        let valid = await this.userService.IsPasswordValid(login, password)
+        if (!valid)
+            return Error('Wrong password');
+
+        let user = await this.userService.GetUser(login);
+        user.password = '';
+
+        let token = jwt.sign(user, process.env.TOKEN_SECRET as string, { expiresIn: '2h' })
+        return token;
+    }
 }
