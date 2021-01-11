c608063e7d6339efdc4fe0de7fc2ea6448b2d243
AlexGerasi <ola.gerasimuk@gmail.com>
2021-01-11 21:08:24 +0100

    home icons close #50
    icons displayed depending on the role

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { IUser } from '../models/iuser';
-import { AuthService } from '../services/auth.service';
+import {Component, OnInit} from '@angular/core';
+import {IUser} from '../models/iuser';
+import {AuthService} from '../services/auth.service';
+import {faTrash, faEdit, faBell, faTasks, faUserCog, faUser} from '@fortawesome/free-solid-svg-icons';
+import {Role} from "../models/role.enum";
+import {User} from "../models/user";
 
 @Component({
   selector: 'app-home',
@@ -8,16 +11,36 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-    currentUser: IUser;
-    constructor(private authenticationService: AuthService) { 
-            this.currentUser = this.authenticationService.currentUserValue;
-        }
+  currentUser: User;
+  faTrash = faTrash;
+  faEdit = faEdit;
+  faBell = faBell;
+  faTasks = faTasks;
+  faUserCog = faUserCog;
+  faUser = faUser;
 
-    ngOnInit(): void {
-    }
 
-    logout()
-    {
-        this.authenticationService.logout("/login");
-    }
+  constructor(public authService: AuthService) {
+    this.authService.currentUser.subscribe(
+      (currentUser: User) => {
+        this.currentUser = currentUser;
+      }
+    );
+  }
+
+  async ngOnInit() {
+    this.currentUser = await this.authService.currentUserValue;
+  }
+
+  get isAdmin() {
+    return this.currentUser && Number(this.currentUser.userRole) === Role.Admin;
+  }
+
+  get isManager() {
+    return this.currentUser && Number(this.currentUser.userRole) === Role.Manager;
+  }
+
+  get isWorker() {
+    return this.currentUser && Number(this.currentUser.userRole) === Role.Worker;
+  }
 }
